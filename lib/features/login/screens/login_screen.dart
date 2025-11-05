6dcbc2db6ca2b826f1ccc06b23342d42057697b9
shahin <sarker4292@gmail.com>
2025-11-05 08:25:49 +0600

    added user input section  and search button

--- lib/features/login/screens/login_screen.dart
+++ lib/features/login/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:inilabs_assignment/controller/theme_controller.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -12,8 +11,40 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GitHub Explorer'),
+        title:  const Text('GitHub Explorer'),
         actions: [IconButton(onPressed: themeController.toggleTheme, icon: Icon(Icons.brightness_6))],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 15,
+            children: [
+              const Text('Enter GitHub Username', style: TextStyle(fontSize: 18)),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'e.g. shahin4292',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  fixedSize: Size(MediaQuery.sizeOf(context).width, MediaQuery.sizeOf(context).height * 0.07)
+                ),
+                onPressed: () {
+                  // Implement login logic here
+                },
+                child:  Text('Search',style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Theme.of(context).colorScheme.onPrimary)),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
