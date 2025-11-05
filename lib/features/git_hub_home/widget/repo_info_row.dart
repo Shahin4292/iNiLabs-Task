f0d2741804dc9a4ec426e70ff44756bc8c3995ac
shahin <sarker4292@gmail.com>
2025-11-05 18:48:33 +0600

    added repoInfo widget

---
+++ lib/features/git_hub_home/widget/repo_info_row.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:inilabs_assignment/utils/dimensions.dart';
+
+class RepoInfoRow extends StatelessWidget {
+  final String text;
+  final String language;
+  final CrossAxisAlignment crossAxisAlignment;
+  final MainAxisAlignment mainAxisAlignment;
+
+  const RepoInfoRow({
+    super.key,
+    required this.text,
+    required this.language,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.mainAxisAlignment = MainAxisAlignment.start,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: crossAxisAlignment,
+      mainAxisAlignment: mainAxisAlignment,
+      children: [
+
+        Icon(Icons.star, size: Dimensions.fontSizeLarge, color: Colors.amber),
+
+         SizedBox(width: Dimensions.paddingSizeExtraSmall),
+
+        Text(text, style: Theme.of(context).textTheme.bodyMedium),
+
+        SizedBox(width: Dimensions.paddingSizeDefault),
+
+         Icon(Icons.code, size: Dimensions.fontSizeLarge),
+
+        SizedBox(width: Dimensions.paddingSizeExtraSmall),
+
+        Text(language, style: Theme.of(context).textTheme.bodyMedium),
+      ],
+    );
+  }
+}
